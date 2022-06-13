ea7e9a0247bfb23920791d283790943d588df240
alzkdpf999@naver.com <alzkdpf999@naver.com>
2022-06-13 21:39:09 +0900

    io

--- namoo.tutorial.io/src/namoo/tutorial/io/FileExample.java
+++ namoo.tutorial.io/src/namoo/tutorial/io/FileExample.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Properties;
-//https://lifeinprogram.tistory.com/19 getProperty 사용법을 위한 사이트
+//	https://lifeinprogram.tistory.com/19 getProperty 사용법을 위한 사이트
 //파일(디렉토리) 정보 조회 및 조작
 public class FileExample {
 
@@ -15,27 +15,44 @@ public class FileExample {
 		Properties pro=System.getProperties(); //hash table 확장 맵 계열 파일저장할때 String 구조로만 
 		Enumeration<Object> e = pro.elements(); //Enumration 열거된 데이터를 저장 value 가져오기
 		Enumeration<Object> keys = pro.keys(); // 키값 가져오기
-	/*	while(e.hasMoreElements()) { //열거된 데이터를 가져올 때 가져올게 있는지 없는지 검사
+		while(e.hasMoreElements()) { //열거된 데이터를 가져올 때 가져올게 있는지 없는지 검사
 			String element =(String) e.nextElement(); //가져올게 있으면 가져와라  (value)
 			System.out.println(element);
 		}
-		*/
-		while(keys.hasMoreElements()) {
+
+
+		while(keys.hasMoreElements()) { 
 			String key=(String) keys.nextElement();
-			String value= pro.getProperty(key);
-			System.out.println(key + " = " + value);
+			String value= pro.getProperty(key); System.out.println(key + " = " + value);
 		}
 
-		File file= new File("sample.txt");
+		//File.separator \을 줌
+		String download = System.getProperty("user.home")+ File.separator+"Downloads";
+		System.out.println(download);
 
-		//		System.out.println("getName():" +file.getName()); //경로명에 있는 마지막 이름
-		//		System.out.println("getParent():" +file.getParent());//경로명에서 마지막 이름 제외한 나머지들
-		//		System.out.println("getPath():" +file.getPath()); //경로명 얻기
-		//		System.out.println("getAbsolutePath():" +file.getAbsolutePath()); // 절대경로명
-		//		System.out.println("getCanonicalPath():" +file.getCanonicalPath());
-		//		System.out.println("length():" +file.length()+"bytes"); // 파일 크기
-		//		System.out.println("isFile():" +file.isFile()); // 파일인지 아닌지
-		//		System.out.println("isDirectory():" +file.isDirectory()); //디렉토리인지 아닌지
+		File file= new File("./sample.txt");
+		boolean ex = file.exists(); //파일이 존재하는지(t) 존재안하는지(f)   
+		System.out.println(ex);
+		System.out.println("getName():" +file.getName()); //경로명에 있는 마지막 이름
+		System.out.println("getParent():" +file.getParent());//경로명에서 마지막 이름 제외한 나머지들
+		System.out.println("getPath():" +file.getPath()); //경로명 얻기
+		System.out.println("getAbsolutePath():" +file.getAbsolutePath()); // 절대경로명
+		System.out.println("getCanonicalPath():" +file.getCanonicalPath()); // 위에꺼와 비슷함 차이는 있으나 큰 차이는 모르겠
+		System.out.println("length():" +file.length()+"bytes"); // 파일 크기
+		System.out.println("isFile():" +file.isFile()); // 파일인지(t) 아닌지(f)
+		System.out.println("isDirectory():" +file.isDirectory()); //디렉토리인지(t) 아닌지(f)
+
+		//파일 목록 조회
+		File directory = new File("./");
+		File[] directoryList = directory.listFiles(); // 배열안에 저장 디렉토리인것만
+		for(File List: directoryList) {
+			String tf = List.isDirectory() ? "[D]" : "[F]"; //디렉토리이면 D 파일이면 F를 출력 
+			if(tf.equals("[F]")) {
+				System.out.println(tf+ List.length() +":"+ List);
+			}
+			System.out.println(tf + " : " + List);
+		}
+		
 	}
 
 }
