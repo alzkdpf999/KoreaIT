531547c7c14a1e77da41dcb3ef1d082c41a5b0f2
alzkdpf999@naver.com <alzkdpf999@naver.com>
2022-06-14 21:52:37 +0900

    0614

--- namoo.tutorial.io/src/namoo/tutorial/io/StandarInOutExample.java
+++ namoo.tutorial.io/src/namoo/tutorial/io/StandarInOutExample.java
@@ -12,13 +12,14 @@ public class StandarInOutExample {
 		System.out.println(System.in);*/
 		//System.out 은 PrintStream class 
 		System.out.print("당신 이름이 뭡니까?:");
-		byte[] buffer = new byte[10];
-		System.in.read(buffer);
-		String name = new String(buffer, 0 , buffer.length);
-		System.out.println(name);
+//		byte[] buffer = new byte[10];
+//		System.in.read(buffer);
+//		String name = new String(buffer, 0 , buffer.length);
+//		System.out.println(name);
 
-		//Scanner sc= new Scanner(System.in); 그냥 연습
-		//String a= sc.nextLine();
-
+		Scanner sc= new Scanner(System.in); //그냥 연습 위에 과정을 다 해줌  꼭 키보드가 아니여도 파일 가능
+		String a= sc.nextLine();
+		int age = sc.nextInt();
+		System.out.println(a+ " : " + age);
 	}
 }
