050c2f00890371927026dc10f6a483ac3059a389
alzkdpf999@naver.com <alzkdpf999@naver.com>
2022-07-04 21:51:33 +0900

    fix

--- namoo.tutorial.JavaScript/js/Object07-04.js
+++ namoo.tutorial.JavaScript/js/Object07-04.js
@@ -4,4 +4,5 @@ let obj1 ={}; //권장
 let str = new Object('hack');
 let num = new Object(10);
 console.log(str.constructor === String);
-console.log(num.constructor === Number);
+console.log(num.constructor === Number
+  );
